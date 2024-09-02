abe8f4d8355a6a1ca06a5a1a2cae25e0b31df908
claudiahW <murungiclaud@gmail.com>
2024-09-02 12:44:30 +0300

    write functions for increment decrement and reset

---
+++ COUNTER/counter.js
@@ -0,0 +1,18 @@
+document
+  .getElementById("incrementBtn")
+  .addEventListener("click", incrementCounter);
+
+document
+  .getElementById("decrementBtn")
+  .addEventListener("click", decrementCounter);
+
+document.getElementById("resetBtn").addEventListener("click", resetCounter);
+
+let counterDisplay = document.getElementById("counterDisplay");
+let countervalue = 0;
+
+function incrementCounter() {}
+
+function decrementCounter() {}
+
+function resetCounter() {}
