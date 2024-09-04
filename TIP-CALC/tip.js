58978d50463ec66149137897be133d5e7da70335
claudiahW <murungiclaud@gmail.com>
2024-09-04 10:03:47 +0300

    initialixe ids of he input and select items

---
+++ TIP-CALC/tip.js
@@ -0,0 +1,10 @@
+function calculateTip() {
+  const billAmountInput = document.getElementById("billAmount");
+  const serviceRatingSelect = document.getElementById("serviceRating");
+  const splitCountType = document.getElementById("splitCount");
+  const mealTypeSelect = document.getElementById("mealType");
+}
+
+const calculateBtn = document
+  .getElementById("calculateBtn")
+  .addEventListener("click", calculateTip);
