c3aa9fb24e8189c367db62b8425c5a338f94792b
claudiahW <murungiclaud@gmail.com>
2024-09-04 19:20:23 +0300

    add a month name and a weekday array

---
+++ TIMEDATE-WIDGET/script.js
@@ -0,0 +1,29 @@
+let dateContainer = document.querySelector(".date-container");
+let hoursContainer = document.querySelector(".hours");
+let minutesContainer = document.querySelector(".minutes");
+let secondsContainer = document.querySelector(".seconds");
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
