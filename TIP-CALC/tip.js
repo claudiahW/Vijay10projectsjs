d92e4de304f1fc16e12e8aee94b8b06a962c01b7
claudiahW <murungiclaud@gmail.com>
2024-09-04 10:56:45 +0300

    add a switch statement to correspond with the quality of service

--- TIP-CALC/tip.js
+++ TIP-CALC/tip.js
@@ -1,8 +1,39 @@
 function calculateTip() {
   const billAmountInput = document.getElementById("billAmount");
   const serviceRatingSelect = document.getElementById("serviceRating");
-  const splitCountType = document.getElementById("splitCount");
+  const splitCountInput = document.getElementById("splitCount");
   const mealTypeSelect = document.getElementById("mealType");
+
+  const tipAmountOutput = document.getElementById("tipAmount");
+  const totalAmountOutput = document.getElementById("totalAmount");
+  const amountPerPersonOutput = document.getElementById("amountPerPerson");
+
+  const billAmount = parseFloat(billAmountInput.value);
+  const serviceRating = parseFloat(serviceRatingSelect.value);
+  const splitCount = parseInt(splitCountInput.value);
+  const mealType = mealTypeSelect.value;
+
+  if (isNaN(billAmount) || isNaN(serviceRating) || isNaN(splitCount)) {
+    tipAmountOutput.textContent = "Please enter a valid number";
+    totalAmountOutput.textContent = "";
+    amountPerPersonOutput.textContent = "";
+    return;
+  }
+  let tip;
+  switch (serviceRating) {
+    case 1:
+      tip = billAmount * 0.05;
+      break;
+    case 2:
+      tip = billAmount * 0.10;
+      break;
+    case 3:
+      tip = billAmount * 0.15;
+      break;
+    case 4:
+      tip = billAmount * 0.20;
+      break;
+  }
 }
 
 const calculateBtn = document
